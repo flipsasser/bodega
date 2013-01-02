a474a064945a212b85cd9cf381626dd9ccec78cb
Flip Sasser <flip@x451.com>
2013-01-02 10:25:03 -0500

    Moved button_to_product helper into regular CartHelper

--- app/helpers/bodega/cart_helper.rb
+++ app/helpers/bodega/cart_helper.rb
@@ -1,15 +1,26 @@
 module Bodega
   module CartHelper
+    def button_to_cart(product, label = 'Add to Cart', options = {})
+      unless options.key? :disabled
+        options[:disabled] = !product.in_stock?
+      end
+      form_tag(bodega.add_path) do
+        hidden_field_tag('product[type]', product.class) +
+        hidden_field_tag('product[id]', product.id) +
+        button_tag(label, options)
+      end
+    end
+
     protected
     def current_order
       @current_order ||= Bodega::Order.new.tap do |order|
-        #begin
+        begin
           if Bodega.config.customer_method
             order.customer = send(Bodega.config.customer_method)
           end
-        #rescue NoMethodError
+        rescue NoMethodError
           raise "Please configure Bodega.config.customer_method to point to a valid method for accessing a customer record (default: current_user)"
-        #end
+        end
         order.order_products = current_products.map do |type, product|
           product = product.symbolize_keys
           OrderProduct.new do |order_product|
