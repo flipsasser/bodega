49ceb6d9865b6932bd877d3199bbb54679c39730
Flip Sasser <flip@inthebackforty.com>
2013-01-30 18:14:24 -0500

    Order tests

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper/active_record'
+require 'bodega/order'
+require 'bodega/order_product'
+require 'bodega/product'
+
+
+describe Bodega::Order do
+  before do
+    TestProduct.send :include, Bodega::Product
+  end
+
+  let!(:product_1) { TestProduct.create!(price: 30) }
+  let!(:product_2) { TestProduct.create!(price: 25) }
+  let(:cart) do
+    {
+      "TestProduct.1" => {
+        type: "TestProduct",
+        id: "1",
+        quantity: "1"
+      },
+      "TestProduct.2" => {
+        type: "TestProduct",
+        id: "2",
+        quantity: "2"
+      }
+    }
+  end
+  let(:order) { Bodega::Order.new }
+
+  describe "#build_products" do
+    it "builds an order from a cart" do
+      order.build_products(cart)
+      order.order_products.size.should == 2
+    end
+  end
+
+  describe "#payment_method" do
+    require 'bodega'
+    require 'bodega/payment_method'
+    class Paypal; end
+
+    it "returns an instance of Bodega::PaymentMethod::Base" do
+      order.payment_method.should be_instance_of(Bodega::PaymentMethod::Paypal)
+    end
+
+    it "returns an instance of Bodega::PaymentMethod::Base with a reference to the order" do
+      order.payment_method.order.should == order
+    end
+  end
+
+  describe "#shipping_method" do
+    require 'bodega'
+    require 'bodega/shipping_method'
+
+    before do
+      Bodega.config { shipping_method :UPS }
+    end
+
+    it "returns an instance of Bodega::ShippingMethod::Base" do
+      order.shipping_method.should be_instance_of(Bodega::ShippingMethod::UPS)
+    end
+
+    it "returns an instance of Bodega::ShippingMethod::Base with a reference to the order" do
+      order.shipping_method.order.should == order
+    end
+  end
+
+  describe "#subtotal" do
+    it "adds up the #order_products subtotals" do
+      order.build_products(cart)
+      order.save!
+      order.subtotal.should == 80
+    end
+  end
+end
